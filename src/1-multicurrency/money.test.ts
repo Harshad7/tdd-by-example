69519bd628cc24a1fadff5828bfe4af0ce5eac3e
Harshad7 <harshadv@buffalo.edu>
2025-03-13 00:17:21 -0700

    chapter-6

--- src/1-multicurrency/money.test.ts
+++ src/1-multicurrency/money.test.ts
@@ -1,4 +1,4 @@
-import { Dollar } from './money'
+import { Dollar, Franc } from './money'
 
 test('Test multiplication', ()=>{
     const five = new Dollar(5);
@@ -6,7 +6,15 @@ test('Test multiplication', ()=>{
     expect(five.times(3)).toEqual(expect.objectContaining(new Dollar(15)));
 })
 
+test('Test Franc multiplication', ()=>{
+    const five = new Franc(5);
+    expect(five.times(2)).toEqual(expect.objectContaining(new Franc(10)));
+    expect(five.times(3)).toEqual(expect.objectContaining(new Franc(15)));
+})
+
 test('Testing equality', ()=> {
     expect(new Dollar(5).equals(new Dollar(5))).toBeTruthy
     expect(new Dollar(6).equals(new Dollar(5))).toBeFalsy
+    expect(new Franc(5).equals(new Franc(5))).toBeTruthy
+    expect(new Franc(6).equals(new Franc(5))).toBeFalsy
 })
--- src/1-multicurrency/money.ts
+++ src/1-multicurrency/money.ts
@@ -1,18 +1,40 @@
-export class Dollar {
-
-    amount : number;
-    public constructor(amount : number){
+export class Money{
+    protected amount! : number;
+    constructor(amount : number) {
         this.amount = amount;
+    }
+    public getAmount(): number{
+        return this.amount
+    }
+    public equals(obj: any): Boolean{
+        const money : Money =  obj as Money;
+        return this.amount === money.getAmount();
+    }
+}
+
+export class Dollar extends Money{
+
+    public constructor(amount : number){
+        super(amount);
     }
     
     public times(multiplier: number) : Dollar | null{
        return new Dollar(this.amount * multiplier);
     }
 
-    public equals(obj: any): Boolean{
-        const dollar: Dollar  =  obj as Dollar;
-        return this.amount === dollar.amount;
+  
+}
 
+export class Franc extends Money {
+    
+    public constructor(amount : number){
+        super(amount);
+    }   
+    
+    public times(multiplier: number) : Franc | null{
+       return new Franc(this.amount * multiplier);
     }
 }
+
+
 
